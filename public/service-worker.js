fbb9f3abd0c667b2ad991dc5cb7464b6f2e232a0
einalem4 <melanie@codehangar.io>
2021-04-26 21:10:29 -0600

    minor fixes to service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -20,23 +20,8 @@ const FILES_TO_CACHE = [
   '/icons/icon-512x512.png'
 ];
 
-// Respond with cached resources
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
-    })
-  )
-});
 
-// adding files to the precache so that the application can use the cache (installing service worker)
+// Cache resources
 self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -44,8 +29,6 @@ self.addEventListener('install', function (e) {
       return cache.addAll(FILES_TO_CACHE);
     })
   );
-
-  self.skipWaiting();
 });
 
 // Cache resources
@@ -66,7 +49,6 @@ self.addEventListener('activate', function (e) {
       );
     })
   )
-  self.clients.claim();
 });
 
 // Delete outdated caches
